38258b80958448eab39823b65a1d378d335aafb2
John Thompson <john@springframework.guru>
2024-04-06 13:15:52 -0400

    initial project add

---
+++ src/test/java/guru/springframework/springaipromptengineering/SpringAiPromptEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springaipromptengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPromptEngineeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
